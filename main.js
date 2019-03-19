0d6cef0067bbd552bc05be152f6fd61d35d965dd
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-03-19 11:13:18 -0700

    Dan created proxy server, now Imgur API is happy :)

--- main.js
+++ main.js
@@ -2,15 +2,23 @@
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.imgur.com/3/gallery/search/{{sort}}/{{window}}/{{page}}?q=yosemite",
-    "method": "GET",
-    "headers": {
-        "Authorization": "Client-ID 96d895f68c7df46",
-        "cache-control": "no-cache",
-        "Postman-Token": "f713ef44-d51f-4bd1-b690-c13fe1577ce3"
+    //"url": "https://api.imgur.com/3/gallery/t/yosemite/{{sort}}/{{window}}/{{page}}",
+    "url": "http://localhost/c219_hackathon2/api/imgurproxy.php",
+    "method": "POST",
+    "dataType": 'JSON',
+    "data":{
+        'refresh_token': 'refresh_token',
+        'client_id': '96d895f68c7df46',
+        'client_secret': '7797522e4aef6c58c534220df74b589a90794cac',
+        'grant_type': 'refresh_token',
     }
+    // "headers": {
+    //     "Authorization": "Client-ID {{clientId}}",
+    //     "cache-control": "no-cache",
+    //     "Postman-Token": "588d1377-9763-44ac-8fa2-7bc5686bfe48"
+    // }
 }
-
+  
 $.ajax(settings).done(function (response) {
     console.log(response);
 });
