61535a1d38fd3961573f48f7b86d60239c62c12f
Eric George <geric738@aol.com>
2019-03-20 18:02:00 -0700

    added comments to main and map js files

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
+/**Declare global variables relating to the map and user choice on landing page */
 var mapObj = null;
 var userPreference = null;
 
+/**attach event handler initializeApp to the event of the document being fully loaded */
 $(document).ready(initializeApp);
+
+/**add click handlers to now existing dom elements */
 function initializeApp(){
     $("#submit_button").click(handleSubmit);
 }
 
+/**function called in the event of the submit button being clicked */
 function handleSubmit(){
-    userPreference = $(".preference_form").val();
+    userPreference = $(".preference_form").val(); /**stores user's choice in a global variable */
+
+    /**clear the screen of existing dom elements, then make the map visible */
     $(".logo").css("display", "none");
     $(".forms_container").css("display", "none");
     $("#map_container").css("display", "block");
+
+    /**instantiate a new map object, add it to the dom, then create the markers */
     var mapObj = new Park_map(userPreference);
     mapObj.addMarkers();
 }
 
+/**function called in the event of one of our 'images', 'weather', or 'news' divs being clicked */
 function handleInfoClicks(){
-    var classes = this.className.split(' ');
+    var classes = this.className.split(' '); /**split the class string up by spaces, effectively retrieving its individual classes */
     var parkName = classes[0];
     // if (classes[1] === 'images') {
     //     var parkName = classes[0];
     //     var imageGallery = new ParkImages(parksList[parkName].imgurTag)
     //     imageGallery.retrieveImages();
     // }
+
+    /**switch statement checking which park was clicked based on the second class of the clicked div */
     switch (classes[1]){
         case 'images':
+        /**create an image modal in the event the user clicked the 'recent imgur posts' div */
             var imageGallery = new ParkImages(parksList[parkName].imgurTag)
             imageGallery.retrieveImages();
             break;
@@ -47,9 +60,11 @@ function handleInfoClicks(){
             };
             break;
         case 'news':
+        /**create news modal in the event the user clicked the 'local news' div */
             console.log('news is work-in-progress');
             var newsAPI = new News(parkName);
             newsAPI.getDataFromServer();
             break;
     }
 }
+
