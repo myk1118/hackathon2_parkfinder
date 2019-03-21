121232c1dcb2d55f4ded4bfb2d33a04dc09444ec
Caroline Pan <abc1588@yahoo.com>
2019-03-20 18:16:16 -0700

    cosmetic fixes and comments

--- weather.js
+++ weather.js
@@ -1,10 +1,19 @@
-//$('button').click(getWeatherData);
+//==============================================================================
+// weather.js
+// purpose: handle all tasks related to weather, including
+//  (1) define weather class object
+//  (2) setup and make API call to OpenWeatherMap to access weather data
+//  (3) process data returned from API call
+//  (4) prepare and display weather data for presentation on webpage
+//==============================================================================
 
-//var yosemite = {lat: 37.8651, lng: -119.5383};
+//=========================== code used for testing purpose only ===========================================
+// $('button').click(getWeatherData);
+// var yosemite = {lat: 37.8651, lng: -119.5383};
 // var lat = 37.8651;
 // var lng = -119.5383;
-
 // api.openweathermap.org/data/2.5/weather?lat=37.8651&lon=-119.5383&APPID=de0df42deb8634d9a313c41fc3ae96b7
+//==========================================================================================================
 
 class WeatherHandler{
 
@@ -19,21 +28,22 @@ class WeatherHandler{
         this.handleError = this.handleError.bind(this);
 
     }
-
+    //make API call to weather data website using AJAX
     getWeatherData(){
         $.ajax({
             url: this.baseUrl + 'lat=' + this.lat + '&lon=' + this.lng + '&APPID=de0df42deb8634d9a313c41fc3ae96b7',
             //success: data=>{this.callback(data);
-            success: this.callback
+            success: this.callback,
+            error: this.handleError
         })
     }
-
+    //retrieve the data returned from the API call
     processWeatherData (data){
         this.w_data = data;
         console.log('inside getWeatherData...', this.w_data);
         displayWeatherData(this.w_data);
     }
-
+    //in case of error, display an error message
     handleError() {
         console.log("OpenWeatherMap - Server Data request Failed");
     }
@@ -44,12 +54,12 @@ class WeatherHandler{
 // var weatherAPI = new WeatherHandler(lat, lng);
 // weatherAPI.getWeatherData();
 
-
+//prepare the data retrieved from API call fro presentation on web page
 function displayWeatherData(w_data) {
     // extract data from w_data object and prep them to display on web page
     // to convert from kelvin to farenheit - subtract 273.15, multiply by 9/5, and add 32
     var place = w_data.name;
-    var wind_speed = w_data.wind.speed;
+    var wind_speed = w_data.wind.speed  + ' mph';;
     var curr_temp = ((w_data.main.temp - 273.15) * 9 / 5 + 32).toFixed(0);
     var min_temp = ((w_data.main.temp_min - 273.15) * 9 / 5 + 32).toFixed(0);
     var max_temp = ((w_data.main.temp_max - 273.15) * 9 / 5 + 32).toFixed(0);
@@ -57,8 +67,11 @@ function displayWeatherData(w_data) {
     var description = w_data.weather[0].description;
     var sunrise = new Date(parseInt(w_data.sys.sunrise));
     var sunset = new Date(parseInt(w_data.sys.sunset));
-    sunrise = sunrise.toLocaleTimeString('en-US',{timeZone:'America/Adak'});
-    sunset = sunset.toLocaleTimeString('en-US',{timeZone:'America/Adak'});
+    // sunrise = sunrise.toLocaleTimeString('en-US',{timeZone:'America/Adak'});
+    // sunset = sunset.toLocaleTimeString('en-US',{timeZone:'America/Adak'});
+    sunrise = sunrise.toLocaleTimeString('en-US');
+    sunset = sunset.toLocaleTimeString('en-US');
+
 
     $('#description').text('Condition: ' + description);
     $('#curr_temp').text('Current Temp: ' + curr_temp + ' F');
